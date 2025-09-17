b2a354ffb1727665049b346bca7010b70c249861
Shaygan Kordestani <shaygan1994@gmail.com>
2025-09-17 13:32:39 +0200

    moved heaader into react

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById("app")).render(<h1>Hello React</h1>);
+function Application() {
+  const [tasks, setTasks] = useState([
+    { description: "Someting" },
+    { description: "Someting else" },
+  ]);
+  return (
+    <>
+      <h1>My tasks</h1>
+      <ul>
+        {tasks.map((t) => (
+          <li>{t.description}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+createRoot(document.getElementById("app")).render(<Application />);
